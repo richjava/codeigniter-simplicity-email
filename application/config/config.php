33e21146b7f4fd666809fef1a02abe783a2f5f55
Richard Lovell <richjavalabs@gmail.com>
2015-03-16 17:34:00 +1300

    Added session config

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/codeigniter-simplicity-todo/';
+$config['base_url']	= 'http://localhost/codeigniter-simplicity-email/';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'sdgsrt2345sdg32456g';
 
 /*
 |--------------------------------------------------------------------------
